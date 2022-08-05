086e72215e747682c8f48295d541a92432d3b05f
WebShining <webshining@protonmail.com>
2022-08-06 01:22:16 +0300

    Update app

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,2 @@
 from .misc import logger
+from .parse import *
--- utils/parse.py
+++ utils/parse.py
@@ -1,17 +1,10 @@
-import requests
+import requests, re
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 
 URL = 'https://holychords.pro'
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'}
-options = Options()
-options.add_argument('headless')
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+CHORDS = ['C', 'D', '']
 
 
 def get_html(url: str):
@@ -23,14 +16,14 @@ def find_songs(name: str):
     soup = BeautifulSoup(get_html(f'{URL}/search?name={name}').text, 'html.parser')
     result = soup.find_all('div', class_='media-body text-truncate')
     items = [r.find('a') for r in result]
-    print([{'href': i['href'][1:], 'name': i.get_text()} for i in items])
+    return [{'href': i['href'][1:], 'name': i.get_text()} for i in items]
 
 
 def get_text(href: str):
-    driver.get(f'{URL}/{href}')
-    driver.find_element(By.XPATH, '//*[@id="main"]/div/div/div[1]/div[2]/div/div[2]/div/div[1]/a[2]').click()
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    print([r.get_text(separator='\n') for r in soup.find_all('div', 'blocks')])
-
-
-get_text('2712')
+    soup = BeautifulSoup(get_html(f'{URL}/{href}').text, 'html.parser')
+    elements = soup.find('pre', id='music_text').get_text(separator='\n').split('\n')
+    text = [re.sub(
+        r'(\d*\s*(Куплет|куплет|(Пред|пред)*Припев|(Пред|пред)*припев|Приспів|приспів|Брідж|брідж|Бридж|бридж):*)',
+        r'\n<b>\1</b>', e) for e in elements if
+        not re.match('(\s{2,}|\s[A-Z]|#|H|A|D|F|E|C|G|Hm|Em|Cm|Am|Bm|Bb|Ab|Eb|Cb)', e)]
+    return text
