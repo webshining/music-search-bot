5bd95d5b7bf7cb47de2c854446c13733b1a47ff9
WebShining <webshining@protonmail.com>
2022-08-14 00:44:51 +0300

    Update app

--- app/handlers/user/__init__.py
+++
@@ -1,4 +0,0 @@
-from .start import dp
-from .help import dp
-
-__all__ = ['dp']
--- app/middlewares/__init__.py
+++
@@ -1,7 +0,0 @@
-from .i18n import i18n
-from .user import UserMiddleware
-
-
-def setup_middlewares(dp):
-    dp.middleware.setup(i18n)
-    dp.middleware.setup(UserMiddleware())
