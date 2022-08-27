1a3866a4a437ee39b5e16af223e34e7569112853
WebShining <webshining@protonmail.com>
2022-08-27 20:43:53 +0300

    Update app

---
+++ app/commands/default.py
@@ -0,0 +1,18 @@
+from aiogram.types import BotCommandScopeDefault, BotCommand
+from loader import bot, i18n, _
+
+
+def get_default_commands(lang: str = 'en'):
+    commands = [
+        BotCommand('/start', _('start bot', locale=lang)),
+        BotCommand('/search', _('search song', locale=lang)),
+        BotCommand('/library', _('library', locale=lang)),
+        BotCommand('/cancel', _('Reset state', locale=lang))
+    ]
+    return commands
+
+
+async def set_default_commands():
+    await bot.set_my_commands(get_default_commands(), scope=BotCommandScopeDefault())
+    for lang in i18n.available_locales:
+        await bot.set_my_commands(get_default_commands(lang), scope=BotCommandScopeDefault(), language_code=lang)
--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from loader import dp
 
 async def on_startup(dispatcher):
     from app.middlewares import setup_middlewares
+    from app.commands import set_default_commands
     setup_middlewares(dp)
+    await set_default_commands()
 
 
 if __name__ == '__main__':
