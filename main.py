46a5824a7cc2a2b2c5880e884e9f02fa081c3dae
WebShining <webshining@protonmail.com>
2022-09-06 15:42:03 +0300

    Update admins notify

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ from aiogram import executor
 from aiogram.types import BotCommandScopeDefault
 from app.middlewares.inter import i18n
 from loader import dp, bot
+from data.config import ADMINS
 
 
 async def on_startup(dispatcher):
     from app.middlewares import setup_middlewares
     from app.commands import set_default_commands
+    for admin_id in ADMINS:
+        try:
+            await bot.send_message(chat_id=admin_id, text='Bot started!')
+        except:
+            pass
     setup_middlewares(dp)
     await set_default_commands()
 
@@ -15,6 +21,11 @@ async def on_shutdown(dispatcher):
     await bot.delete_my_commands(scope=BotCommandScopeDefault())
     for lang in i18n.available_locales:
         await bot.delete_my_commands(scope=BotCommandScopeDefault(), language_code=lang)
+    for admin_id in ADMINS:
+        try:
+            await bot.send_message(chat_id=admin_id, text='Bot shutting down!')
+        except:
+            pass
 
 
 if __name__ == '__main__':
