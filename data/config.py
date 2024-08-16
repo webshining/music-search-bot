9181a583edab5360fa355c71efb423b9287d8fd7
webshining <webshining@protonmail.com>
2024-08-16 16:51:27 +0300

    added redis username and redis password

--- data/config.py
+++ data/config.py
@@ -14,10 +14,15 @@ TELEGRAM_BOT_TOKEN = env.str("TELEGRAM_BOT_TOKEN", None)
 RD_DB = env.int("RD_DB", None)
 RD_HOST = env.str("RD_HOST", None)
 RD_PORT = env.int("RD_PORT", None)
+RD_USER = env.str("RD_USER", None)
+RD_PASS = env.str("RD_PASS", None)
 
 RD_URI = env.str("RD_URI", None) or env.str("REDISCLOUD_URL", None)
 if not RD_URI and RD_DB and RD_PORT and RD_HOST:
-    RD_URI = f"redis://{RD_HOST}:{RD_PORT}/{RD_DB}"
+    url = "redis://"
+    if RD_USER and RD_PASS:
+        url += f"{RD_USER}:{RD_PASS}@"
+    url += f"redis://{RD_HOST}:{RD_PORT}/{RD_DB}"
 
 
 SURREAL_NS = env.str("SURREAL_NS", "test")
